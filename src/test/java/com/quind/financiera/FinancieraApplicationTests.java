e118d55761bbeaf9bbf1112df4cee231c47424b2
sandra-yanza <sandrayanza.study@gmail.com>
2024-07-19 06:19:38 -0500

    Initial commit
    Starter project with Spring Initializr

---
+++ src/test/java/com/quind/financiera/FinancieraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quind.financiera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancieraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
